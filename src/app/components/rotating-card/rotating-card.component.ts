13ae0e5cf51c1cc5456331cc5dc2774c7d50a3b9
aymensellaouti <aymen.sellaouti@gmail.com>
2023-09-04 14:44:31 +0100

    end binding

--- src/app/components/rotating-card/rotating-card.component.ts
+++ src/app/components/rotating-card/rotating-card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-rotating-card',
-  templateUrl: './rotating-card.component.html',
-  styleUrls: ['./rotating-card.component.css']
+  selector: "app-rotating-card",
+  templateUrl: "./rotating-card.component.html",
+  styleUrls: ["./rotating-card.component.css"],
 })
 export class RotatingCardComponent {
-
+  name = "sellaouti";
+  firstname = "aymen";
+  job = "teacher";
+  path = "rotating_card_profile3.png";
 }
